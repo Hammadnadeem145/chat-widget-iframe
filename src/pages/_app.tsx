09ad50808709348aafac5f7188b42fa9f2b40034
Hammad Nadeem <hammadnadeem145@gmail.com>
2024-04-02 20:11:01 +0500

    remove-title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,5 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <div className="container mx-auto">
-      <h1 className="text-3xl font-bold text-center my-8">
-        Welcome to My Website
-      </h1>
-      <Component {...pageProps} />;
-    </div>
-  );
+  return <Component {...pageProps} />;
 }
