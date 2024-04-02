4e4a02760a889c6a2bc286d9b2307bf7999e6009
Hammad Nadeem <hammadnadeem145@gmail.com>
2024-04-02 06:51:46 +0500

    chat-widget-complete

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,12 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-3xl font-bold text-center my-8">
+        Welcome to My Website
+      </h1>
+      <Component {...pageProps} />;
+    </div>
+  );
 }
